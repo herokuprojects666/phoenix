4c709b29da9022a8cc0d0a807d5a4ba8fc7aa9be
Adam Cragg <acragg@kochava.com>
2017-09-21 17:51:12 -0700

    adding room:accounts channel

--- lib/kochava_web/channels/user_socket.ex
+++ lib/kochava_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule KochavaWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", KochavaWeb.RoomChannel
+  channel "room:*", KochavaWeb.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
